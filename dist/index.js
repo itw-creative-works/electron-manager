747eba3f7668d2c5e7250c7bef36c91a566c6719
Ian <ian.wiedenman@gmail.com>
2021-07-16 14:02:19 -0700

    0.0.4: Refactor to work with Electron 13. Breaking changes with the .is*  properties. Now includes a em.properties() method

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@
 */
 const { exec, execFile } = require('child_process');
 let jetpack;
+let electronIsDev;
 
 /*
   ElectronManager Class
@@ -10,34 +11,33 @@ let jetpack;
 function ElectronManager(options) {
   const self = this;
 
+  self.require = require;
+
   self.options = options || {};
   self.options.appName = self.options.appName || '';
   self.options.appId = self.options.appId || self.options.appName.toLowerCase() || '';
 
   // Export libraries for use here
   self._libraries = {};
-  self._libraries.electron = self.options.electron || require('electron');
-  // if (self._libraries.electron.remote) {
+  self.electron = self.options.electron || require('electron');
+
+  // console.log("require('lodash')", require('lodash'));
+  // try {
+  //   self.remote = self.options.remote || require('@electron/remote');
+  // } catch (e) {
+  //   // self.remote = self.options.remote || require('@electron/remote/main');
+  //   console.warn("Couldn't load remote, we are in main process.");
+  // }
+  // if (self.electron.remote) {
   //   self.process = 'renderer';
-  //   self._libraries.electron = self._libraries.electron.remote;
+  //   self.electron = self.electron.remote;
   // } else {
   //   self.process = 'main';
   // }
 
   // Properties
-  self.environment = require('electron-is-dev') ? 'development' : 'production';
-  self.isLinux = process.platform !== 'darwin' && process.platform !== 'win32';
-  self.isSnap = self.isLinux ? require('electron-is-snap').isSnap : false;
-  // self.isRenderer = require('is-electron-renderer')();
-  if (process.mas) {
-    self.storeName = 'mac';
-  } else if (process.windowsStore) {
-    self.storeName = 'windows';
-  } else if (self.isSnap) {
-    self.storeName = 'snap';
-  } else {
-    self.storeName = 'none';
-  }
+  // self.properties().isDevelopment() = require('electron-is-dev') ? 'development' : 'production';
+
 }
 
 /*
@@ -50,8 +50,8 @@ ElectronManager.prototype.app = function() {
       options = options || {};
       options.appId = options.appId || self.options.appId;
       // options.appName = options.appName || self.options.appName;
-      self._libraries.electron.app.setAsDefaultProtocolClient(protocol);
-      if (self.isLinux) {
+      self.electron.app.setAsDefaultProtocolClient(protocol);
+      if (self.properties().isLinux()) {
         await asyncCmd(`xdg-mime default ${options.appId}.desktop "x-scheme-handler/${protocol}"`).catch(e => console.error);
         await asyncCmd(`xdg-settings set default-url-scheme-handler ${protocol} ${options.appId}`).catch(e => console.error);
       }
@@ -60,9 +60,9 @@ ElectronManager.prototype.app = function() {
     },
     getApplicationNameForProtocol: async (protocol) => {
       protocol = protocol.split('://')[0];
-      const nativeCheck = self._libraries.electron.app.getApplicationNameForProtocol(`${protocol}://`);
+      const nativeCheck = self.electron.app.getApplicationNameForProtocol(`${protocol}://`);
       let linuxCheck;
-      if (self.isLinux) {
+      if (self.properties().isLinux()) {
         linuxCheck = await asyncCmd(`xdg-settings get default-url-scheme-handler ${protocol}`)
           .catch(e => {
             console.error(e);
@@ -76,10 +76,10 @@ ElectronManager.prototype.app = function() {
       options = options || {};
       options.appId = options.appId || self.options.appId;
       // options.appName = options.appName || self.options.appName;
-      const comparator = (self.environment === 'development') ? 'electron' : options.appId.toLowerCase();
-      const nativeCheck = self._libraries.electron.app.isDefaultProtocolClient(protocol);
+      const comparator = (self.properties().isDevelopment()) ? 'electron' : options.appId.toLowerCase();
+      const nativeCheck = self.electron.app.isDefaultProtocolClient(protocol);
       let linuxCheck;
-      if (self.isLinux) {
+      if (self.properties().isLinux()) {
         linuxCheck = await asyncCmd(`xdg-settings get default-url-scheme-handler ${protocol}`)
           .catch(e => {
             console.error(e);
@@ -98,9 +98,9 @@ ElectronManager.prototype.app = function() {
       const appName = options.appName || self.options.appName;
       delete options.appName;
 
-      self._libraries.electron.app.setLoginItemSettings(options);
+      self.electron.app.setLoginItemSettings(options);
 
-      if (self.isLinux) {
+      if (self.properties().isLinux()) {
         if (!self._libraries.linuxAutoLauncher) {
           const AutoLaunch = require('auto-launch');
           self._libraries.linuxAutoLauncher = new AutoLaunch({
@@ -123,7 +123,7 @@ ElectronManager.prototype.app = function() {
     // Custom methods
     // getAlternateAppId: async (protocol) => {
     //   const alternateId = await self.app().getApplicationNameForProtocol(protocol)
-    //   if (self.isLinux) {
+    //   if (self.properties().isLinux()) {
     //
     //   } else {
     //     return self.options.appId;
@@ -141,7 +141,7 @@ ElectronManager.prototype.app = function() {
           await executeFile(options.setUserFTAPath, ['http', `Applications\\${options.appName}.exe`]).catch(e => console.error);
           await executeFile(options.setUserFTAPath, ['https', `Applications\\${options.appName}.exe`]).catch(e => console.error);
         }
-      } if (self.isLinux) {
+      } if (self.properties().isLinux()) {
         await asyncCmd(`xdg-settings set default-web-browser ${options.appId}.desktop`).catch(e => console.error)
       }
       // console.log('setAsDefaultBrowser', options);
@@ -149,7 +149,7 @@ ElectronManager.prototype.app = function() {
     isDefaultBrowser: async (options) => {
       options = options || {};
       options.appId = options.appId || self.options.appId;
-      const comparator = (self.environment === 'development') ? 'electron' : options.appId.toLowerCase();
+      const comparator = (self.properties().isDevelopment()) ? 'electron' : options.appId.toLowerCase();
       const matchesApplication =
         `${await self.app().getApplicationNameForProtocol('http://')}`.toLowerCase().includes(comparator)
         || `${await self.app().getApplicationNameForProtocol('https://')}`.toLowerCase().includes(comparator)
@@ -158,7 +158,7 @@ ElectronManager.prototype.app = function() {
         || `${await self.app().isDefaultProtocolClient('https')}`.toLowerCase().includes(comparator)
       let linuxCheck;
 
-      if (self.isLinux) {
+      if (self.properties().isLinux()) {
         linuxCheck = await asyncCmd(`xdg-settings get default-web-browser`)
           .catch(e => {
             console.error(e);
@@ -176,12 +176,12 @@ ElectronManager.prototype.app = function() {
       options = options || {};
       options.threshold = typeof options.threshold === 'undefined' ? 120 : options.threshold;
 
-      const nativeCheck = self._libraries.electron.app.getLoginItemSettings().wasOpenedAtLogin;
+      const nativeCheck = self.electron.app.getLoginItemSettings().wasOpenedAtLogin;
       const argCheck = process.argv.filter(a => a.includes('--was-opened-at-login')).length > 0;
       let specialCheck;
 
       // Special use cases for these... 'special' platforms
-      if (process.windowsStore || self.isLinux) {
+      if (process.windowsStore || self.properties().isLinux()) {
         const os = require('os');
         const username = os.userInfo().username;
         const moment = require('moment');
@@ -203,6 +203,42 @@ ElectronManager.prototype.app = function() {
       // console.log('wasOpenedAtLogin', options, nativeCheck, argCheck, specialCheck);
       return nativeCheck || argCheck || specialCheck || false;
     }
+  }
+};
+
+
+ElectronManager.prototype.properties = function() {
+  const self = this;
+  return {
+    isDevelopment: function () {
+      // let electronIsDev;
+      // console.log('---self.remote', self.remote);
+      // if (self.remote) {
+      //   electronIsDev = self.remote.require('electron-is-dev')
+      // } else {
+      //   electronIsDev = require('electron-is-dev')
+      // }
+      // return electronIsDev;
+      return self.require('electron-is-dev')
+    },
+    isLinux: function () {
+      return process.platform !== 'darwin' && process.platform !== 'win32';
+    },
+    isSnap: function () {
+      return self.properties().isLinux() ? require('electron-is-snap').isSnap : false;
+    },
+    isStore: function () {
+      if (process.mas) {
+        return 'mac';
+      } else if (process.windowsStore) {
+        return 'windows';
+      } else if (self.properties().isSnap()) {
+        return 'snap';
+      } else {
+        return false;
+      }
+    },
+
   }
 };
 
