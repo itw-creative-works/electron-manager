da02152a1d9bd205ab7afda4d0876d8083ffc994
Ian <ian.wiedenman@gmail.com>
2024-06-27 03:46:52 -0700

    Update notary

--- dist/_temp/afterSign copy.js
+++ dist/_temp/afterSign copy.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const { notarize } = require(path.resolve(process.cwd(), 'node_modules', '@electron/notarize'));
+// const Manager = new (require('electron-manager'))();
 const Manager = new (require(path.resolve(process.cwd(), 'node_modules', 'electron-manager')))();
 const chalk = Manager.require('chalk');
 const scriptName = '[afterSign.js]';
@@ -10,22 +11,37 @@ exports.default = async function (context) {
     const appName = context.packager.appInfo.productFilename;
     const appPath = path.join(context.appOutDir, `${appName}.app`);
     const appBundleId = context.packager.config.appId;
+    const notarizationMethod = process.env.APPLE_NOTARIZATION_METHOD ? process.env.APPLE_NOTARIZATION_METHOD : 'legacy';
 
     if (context.packager.info.options.publish !== 'always') {
       return console.log(chalk.blue(scriptName, `Skipping notarization/signing because this is not a publish.`));
     }
 
-    console.log(chalk.blue(scriptName, `Notarizing:`), {appName: appName, appBundleId: appBundleId, appPath: appPath});
+    console.log(chalk.blue(scriptName, `Notarizing:`), {notarizationMethod: notarizationMethod, appName: appName, appBundleId: appBundleId, appPath: appPath});
 
-    await notarize({
-      appPath: appPath,
-      appleId: process.env.APPLE_ID,
-      appleIdPassword: process.env.APPLE_PASSWORD || `@keychain:apple-password`,
-      teamId: process.env.APPLE_TEAM_ID,
-    })
-    .catch(e => {
-      return error(e)
-    })
+    if (notarizationMethod === 'legacy') {
+      await notarize({
+        tool: notarizationMethod,
+        appPath: appPath,
+        appBundleId: appBundleId,
+        appleId: process.env.APPLE_ID,
+        appleIdPassword: process.env.APPLE_PASSWORD || `@keychain:apple-password`,
+      })
+      .catch(e => {
+        return error(e)
+      })
+    } else {
+      await notarize({
+        tool: notarizationMethod,
+        appPath: appPath,
+        appleId: process.env.APPLE_ID,
+        appleIdPassword: process.env.APPLE_PASSWORD || `@keychain:apple-password`,
+        teamId: process.env.APPLE_TEAM_ID,
+      })
+      .catch(e => {
+        return error(e)
+      })
+    }
 
     console.log(chalk.green(scriptName, `Done notarizing: ${appPath}`));
   } else {
